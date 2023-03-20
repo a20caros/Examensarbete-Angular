050af12c53bb9bcb0c28e8f9ea151af44685a33e
a20caros <a20caros@student.his.se>
2023-03-20 12:08:12 +0100

    Created the women's perfumes component and page.
    Created the women's perfumes component and page. Added the path to the page through routes and path which I could then add the path to the page in the navbar so you can navigate to it. Also added the router outlet to allow the content of the page to be displayed

---
+++ src/app/womenPerfyme/showWomenPerfyme.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-womenPerfyme',
+    templateUrl: './showWomenPerfume.component.html',
+    styleUrls: ['./showWomenPerfume.component.css']
+})
+
+export class WomenPerfumeComponent{
+
+}
