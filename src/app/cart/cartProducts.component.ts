7c9d69d64d7114c787a0b134765d8d61f2623be7
a20caros <a20caros@student.his.se>
2023-03-22 11:43:36 +0100

    Created total price in cart
    Created functionality to add up the products in the shopping cart costs to see the total price. Added a function in cartProduct that loops through all products that are in cartItem and adds their prices to variable cartTotalPrice which is then returned. Then prints the total price on the shopping cart page. Also added styling.

--- src/app/cart/cartProducts.component.ts
+++ src/app/cart/cartProducts.component.ts
@@ -1,5 +1,12 @@
 import { CartItem } from "./cartitem.component";
-
 export class Cart{
     items:CartItem[] = [];
+    
+    get cartTotalPrice(): number{
+        let cartTotalPrice = 0; 
+        this.items.forEach(item =>{
+            cartTotalPrice += item.product.price;
+        });
+        return cartTotalPrice;
+    }
 }
