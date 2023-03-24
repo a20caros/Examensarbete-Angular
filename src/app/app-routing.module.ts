f66d69303659ca67c5dcfe79e428b3bf9c9489f5
a20caros <a20caros@student.his.se>
2023-03-24 16:05:22 +0100

    Added a page detailing all the images used on the page
    Added a page detailing all the images used on the page. Created a new component containing the page etc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
 import { WriteProducts } from './generateProducts/generatePerfumes.component';
 import { WriteMenProducts } from './generateMenProducts/generateMenPerfume.component';
+import { ImagesInformationComponent } from './imagesInformation/imagesInformation.component';
 
 const routes: Routes = [
   {path: 'womenPerfume', component:WriteProducts},
   {path: 'cart', component:CartComponent},
   {path: 'MenPerfumes', component:WriteMenProducts},
+  {path: 'ImagesInformationComponent', component:ImagesInformationComponent},
 ];
 
 @NgModule({
---
+++ src/app/imagesInformation/imagesInformation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-imagesInformationComponent',
+    templateUrl: './imagesInformation.component.html',
+    styleUrls: ['./imagesInformation.component.css']
+ 
+})
+
+export class ImagesInformationComponent{
+
+}
