3aab9a2102d69f684dc66d181d8acdd36ba13f7f
a20caros <a20caros@student.his.se>
2023-04-19 10:37:50 +0200

    Added some comments
    Added some comments

--- src/app/createMenProducts/menProducts.service.ts
+++ src/app/createMenProducts/menProducts.service.ts
@@ -6,13 +6,16 @@ import { HttpClient } from '@angular/common/http';
 })
 
 export class ProductMenService {
+    //Get the josn-file
     private apiUrl = 'assets/menProduct.json';
    
     constructor(private http: HttpClient) {}
       getProducts() {
+        //Returns the data in a HTTP GET request
         return this.http.get(this.apiUrl);
       }
       saveData(key: string, data: any) {
+        //Save the products in localStorage so the script can retrieve it
         let menDataString = JSON.stringify(data);
         localStorage.setItem(key, menDataString);
       }
--- src/app/createProducts/products.service.ts
+++ src/app/createProducts/products.service.ts
@@ -6,13 +6,15 @@ import { HttpClient } from '@angular/common/http';
 })
 
 export class ProductService {  
-    private apiUrl = 'assets/womenProduct.json';
+    //Get the josn-file
+    private apiUrl = 'assets/womenProducts.json';
    
     constructor(private http: HttpClient) {}
       getProducts() {
         return this.http.get(this.apiUrl);
       }
       saveData(key: string, data: any) {
+        //Save the products in localStorage so the script can retrieve it
         let dataString = JSON.stringify(data);
         localStorage.setItem(key, dataString);
       }
